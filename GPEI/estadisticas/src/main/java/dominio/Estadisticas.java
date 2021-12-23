8167488dea79b4a5919c0a29799c9f3997464d6f
alvvvr <alvvvr@aone>
2021-12-23 17:53:17 +0100

    Added statistics menu

--- GPEI/estadisticas/src/main/java/dominio/Estadisticas.java
+++ GPEI/estadisticas/src/main/java/dominio/Estadisticas.java
@@ -1,5 +1,90 @@
 package dominio;
 
-public class Estadisticas {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import persistencia.Agente;
+
+public class Estadisticas {
+	
+	public static ArrayList<String> estadoPersona() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet resultSetPersonas;
+		
+		try {
+			resultSetPersonas = Agente.getAgente().select("select ESTADO, count(*) as PERSONAS from PERSONAS group by ESTADO;");
+			while(resultSetPersonas.next()) {
+				list.add(resultSetPersonas.getString(1));
+				list.add(""+resultSetPersonas.getInt(2));
+			}
+			
+		}
+		 catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		return list;
+	}
+	
+	public static ArrayList<String> estadoEnfermedad() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet resultSetEnfermedades;
+		
+		try {
+			
+			resultSetEnfermedades = Agente.getAgente().select("select ESTADO, count(distinct ID_ENFERMEDAD) from PERSONAS join PERSONAS_ENFERMEDAD where PERSONAS.DNI = PERSONAS_ENFERMEDAD.DNI_PERSONA group by ESTADO order by ESTADO;");
+			while(resultSetEnfermedades.next()) {
+				list.add(resultSetEnfermedades.getString(1));
+				list.add(""+resultSetEnfermedades.getInt(2));
+			}
+		}
+		 catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		return list;
+	}
+	
+	public static int nEnfCatalogadas() {
+		int nEnfermedades = 0;
+		ResultSet resultSet;
+		
+		try {
+			
+			resultSet = Agente.getAgente().select("select count(*) from ENFERMEDADES;");
+			if(resultSet.next()) nEnfermedades = resultSet.getInt(1);
+		}
+		 catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		return nEnfermedades;
+	}
+	
+	public static ArrayList<String> nCampaniasEnfermedad() {
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet resultSet;
+		
+		try {
+			resultSet = Agente.getAgente().select("select (select NOMBRE from ENFERMEDADES where ID_ENFERMEDAD = CAMPANIAS.ID_ENFERMEDAD) as ENFERMEDAD, count(*) from CAMPANIAS group by ENFERMEDAD;");
+			while(resultSet.next()) {
+				list.add(resultSet.getString(1));
+				list.add(""+resultSet.getInt(2));
+			}
+		}
+		 catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		return list;
+	}
 }
--- GPEI/estadisticas/src/main/java/presentacion/UI_Estadisticas.java
+++ GPEI/estadisticas/src/main/java/presentacion/UI_Estadisticas.java
@@ -1,5 +1,106 @@
 package presentacion;
 
-public class UI_Estadisticas {
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import dominio.Estadisticas;
+import presentacion.JFrameEstados;
+
+public class UI_Estadisticas extends JFrame {
+	private JPanel contentPane;
+	private DefaultTableModel model;
+	private JTable table;
+	JComboBox<String> cbEstadistica;
+	
+	/* Main function */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI_Estadisticas frame = new UI_Estadisticas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public UI_Estadisticas() {
+		setTitle("Estadísticas");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 438, 385);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("Menu estadísticas");
+		lblTitulo.setFont(new Font(null, Font.PLAIN, 16));
+		lblTitulo.setBounds(150, 37, 150, 30);
+		contentPane.add(lblTitulo);
+		
+		JLabel lblEstadistica = new JLabel("Estadística: ");
+		lblEstadistica.setBounds(110, 85, 250, 30);
+		contentPane.add(lblEstadistica);
+		
+		cbEstadistica =new JComboBox<String>();
+		cbEstadistica.addItem("1. Nº Enfermedades y personas por estado");
+		cbEstadistica.addItem("2. Nº de enfermedades catalogadas");
+		cbEstadistica.addItem("3. Nº de campañas informativas/enfrmedad");
+		cbEstadistica.setBounds(110, 110, 400, 28);
+        contentPane.add(cbEstadistica);
+        
+        final JButton btnBuscarEstados = new JButton("Listar estadísticas");
+        
+        btnBuscarEstados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String selection = String.valueOf(cbEstadistica.getSelectedItem());
+				
+				if(selection.equals("1. Nº Enfermedades y personas por estado")) {
+					JFrameEstados frameEstados = new JFrameEstados();
+					frameEstados.setVisible(true);
+				} else if(selection.equals("2. Nº de enfermedades catalogadas")) {
+					JFrameEnfCatalogadas frameEnfCatalogadas = new JFrameEnfCatalogadas();
+					frameEnfCatalogadas.setVisible(true);
+				} else if(selection.equals("3. Nº de campañas informativas/enfrmedad")) {
+					JFrameCampaniasEnf frameCampanias = new JFrameCampaniasEnf();
+					frameCampanias.setVisible(true);
+				}
+				
+		        /*switch (selection) {
+				case "1. Nº Enfermedades y personas por estado":
+					JFrameEstados frameEstados = new JFrameEstados();
+					frameEstados.setVisible(true);
+					break;
+				case "2. Nº de enfermedades catalogadas":
+					JFrameEnfCatalogadas frameEnfCatalogadas = new JFrameEnfCatalogadas();
+					frameEnfCatalogadas.setVisible(true);
+					break;
+				case "3. Nº de campañas informativas/enfrmedad":
+					JFrameCampaniasEnf frameCampanias = new JFrameCampaniasEnf();
+					frameCampanias.setVisible(true);
+					break;
+				default:
+					break;
+				}*/
+			}
+		});
+		
+		btnBuscarEstados.setBounds(110, 140, 400, 40);
+		contentPane.add(btnBuscarEstados);
+	}
 }
